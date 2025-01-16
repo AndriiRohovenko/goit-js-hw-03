42b7b832e03b05ff2a9bf6be5f6b6e424b18a38f
Andrii.Rohovenko <andrii.rohovenko@gmail.com>
2025-01-17 00:30:27 +0200

    add hw-03

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,17 @@
 'use strict';
 
-function getElementWidth(content, padding, border) {
-  const content_size = parseFloat(content);
-  const padding_size = parseFloat(padding) * 2;
-  const border_size = parseFloat(border) * 2;
-  return content_size + padding_size + border_size;
+function filterArray(numbers, value) {
+  let match_arr = [];
+  for (const number of numbers) {
+    if (number > value) {
+      match_arr.push(number);
+    }
+  }
+  return match_arr;
 }
 
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
